2b7147c84252aa4f68c6417b55f356cfe0623acd
vromanyu <vromanyu@outlook.com>
2026-04-01 19:47:13 +0300

    commit initial scaffolding

---
+++ upload-ms/src/main/java/com/vromanyu/upload/UploadMsApplication.java
@@ -0,0 +1,13 @@
+package com.vromanyu.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadMsApplication.class, args);
+    }
+
+}
