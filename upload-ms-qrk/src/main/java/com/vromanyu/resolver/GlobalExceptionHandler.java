85b3d7364ce72fc3c350214d659a820c5296b978
vromanyu <vromanyu@outlook.com>
2026-04-04 00:39:51 +0300

    add GlobalExceptionHandler using JAX-RS way

--- upload-ms-qrk/src/main/java/com/vromanyu/resolver/GlobalExceptionHandler.java
+++ upload-ms-qrk/src/main/java/com/vromanyu/resolver/GlobalExceptionHandler.java
@@ -1,28 +1,25 @@
 package com.vromanyu.resolver;
 
 import com.vromanyu.dto.ProblemResponse;
-import com.vromanyu.exception.FileNameNotMatchException;
 import io.vertx.core.http.HttpServerRequest;
-import jakarta.validation.ConstraintViolationException;
-import org.jboss.resteasy.reactive.RestResponse;
-import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 
 import java.time.OffsetDateTime;
 import java.util.Collections;
-import java.util.List;
 
-public class GlobalExceptionHandler {
+@Provider
+public class GlobalExceptionHandler implements ExceptionMapper<Exception> {
 
-    @ServerExceptionMapper
-    public RestResponse<ProblemResponse> handleValidationException(ConstraintViolationException e, HttpServerRequest request) {
-        List<String> errorMessages = e.getConstraintViolations().stream().map(cv -> cv.getPropertyPath().toString().split("\\.")[2] + " " + cv.getMessage()).toList();
-        return RestResponse.status(RestResponse.Status.BAD_REQUEST,
-                new ProblemResponse(request.uri(), errorMessages, OffsetDateTime.now(), RestResponse.Status.BAD_REQUEST.getStatusCode()));
-    }
+    @Inject
+    HttpServerRequest request;
 
-    @ServerExceptionMapper
-    public RestResponse<ProblemResponse> handleFileNameNotMatchException(FileNameNotMatchException e, HttpServerRequest request) {
-        return RestResponse.status(RestResponse.Status.BAD_REQUEST,
-                new ProblemResponse(request.uri(), Collections.singletonList(e.getMessage()), OffsetDateTime.now(), RestResponse.Status.BAD_REQUEST.getStatusCode()));
+    @Override
+    public Response toResponse(Exception exception) {
+        String requestUri = request.uri();
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ProblemResponse(
+                requestUri, Collections.singletonList(exception.getMessage()), OffsetDateTime.now(), Response.Status.INTERNAL_SERVER_ERROR.getStatusCode())).build();
     }
 }
