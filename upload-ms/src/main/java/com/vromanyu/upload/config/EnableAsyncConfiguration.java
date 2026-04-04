4bb21e075245b5c8b3b9203e3bdb6642437a6c6f
vromanyu <vromanyu@outlook.com>
2026-04-04 18:41:43 +0300

    enable async support

---
+++ upload-ms/src/main/java/com/vromanyu/upload/config/EnableAsyncConfiguration.java
@@ -0,0 +1,9 @@
+package com.vromanyu.upload.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class EnableAsyncConfiguration {
+}
